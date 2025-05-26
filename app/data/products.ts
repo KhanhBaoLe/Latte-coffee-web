a5263e0c36f8ad24a5f2d341a9dd5d1ca8e0ca2f
DungGiaIT <dung.nguyen230208@vnuk.edu.vn>
2025-05-26 23:50:46 +0700

    update detail and sync data

--- app/data/products.ts
+++ app/data/products.ts
@@ -1,4 +1,3 @@
-// data/products.ts
 export interface Product {
   id: string
   title: string
@@ -6,21 +5,77 @@ export interface Product {
   price: number
   originalPrice?: number
   sizes: string[]
+  milkOptions: string[]
+  drinkOptions: string[]
   toppings: string[]
   basePrices: Record<string, number>
   rating: number
   reviews: number
+  image: string
 }
 
-export const featuredProduct: Product = {
-  id: "midnight-matchalotte",
-  title: "Midnight Matchalotte",
-  description: "Experience the ceremonial-grade matcha blended with creamy oat milk and a touch of honey. Crafted to perfection for your midnight cravings.",
-  price: 7.50,
-  originalPrice: 9.00,
-  sizes: ['S', 'M', 'L'],
-  toppings: ['Whipped Cream', 'Caramel Drizzle', 'Vanilla Foam', 'Chocolate Shavings'],
-  basePrices: { S: 6.50, M: 7.50, L: 8.50 },
-  rating: 4.8,
-  reviews: 128
-}
+export const products = [
+  {
+    id: "1",
+    title: "Midnight Matchalotte",
+    description: "Experience the ceremonial-grade matcha blended with creamy oat milk and a touch of honey.",
+    price: 7.50,
+    originalPrice: 9.00,
+    sizes: ['S', 'M', 'L'],
+    milkOptions: ["Whole Milk", "Skim Milk", "Almond Milk", "Soy Milk"],
+    drinkOptions: ["Hot", "Iced", "Blended"],
+    toppings: ['Whipped Cream', 'Caramel Drizzle', 'Vanilla Foam', 'Chocolate Shavings'],
+    basePrices: { S: 6.50, M: 7.50, L: 8.50 },
+    rating: 4.8,
+    reviews: 128,
+    image: "/images/matchalate.webp"
+  },
+  {
+    id: "2",
+    title: "Midnight Mint Mocha Frappuccino",
+    description: "Rich coffee blended with cool mint and chocolatey chips.",
+    price: 4.3,
+    sizes: ['S', 'M', 'L'],
+    milkOptions: ["Whole Milk", "Skim Milk", "Almond Milk", "Soy Milk"],
+    drinkOptions: ["Hot", "Iced", "Blended"],
+    toppings: ["Whipped Cream", "Chocolate Drizzle", "Mint Syrup"],
+    basePrices:{ S: 3.8, M: 4.3, L: 4.8 },
+    rating: 4.7,
+    reviews: 95,
+    image: "/images/matchalate.webp"
+  },
+  {
+    id: "3",
+    title: "Midnight Matchalotte",
+    description: "Experience the ceremonial-grade matcha blended with creamy oat milk and a touch of honey.",
+    price: 7.50,
+    originalPrice: 9.00,
+    sizes: ['S', 'M', 'L'],
+    milkOptions: ["Whole Milk", "Skim Milk", "Almond Milk", "Soy Milk"],
+    drinkOptions: ["Hot", "Iced", "Blended"],
+    toppings: ['Whipped Cream', 'Caramel Drizzle', 'Vanilla Foam', 'Chocolate Shavings'],
+    basePrices: { S: 6.50, M: 7.50, L: 8.50 },
+    rating: 4.8,
+    reviews: 128,
+    image: "/images/matchalate.webp"
+  },
+  {
+    id: "4",
+    title: "Midnight Matchalotte",
+    description: "Experience the ceremonial-grade matcha blended with creamy oat milk and a touch of honey.",
+    price: 7.50,
+    originalPrice: 9.00,
+    sizes: ['S', 'M', 'L'],
+    milkOptions: ["Whole Milk", "Skim Milk", "Almond Milk", "Soy Milk"],
+    drinkOptions: ["Hot", "Iced", "Blended"],
+    toppings: ['Whipped Cream', 'Caramel Drizzle', 'Vanilla Foam', 'Chocolate Shavings'],
+    basePrices: { S: 6.50, M: 7.50, L: 8.50 },
+    rating: 4.8,
+    reviews: 128,
+    image: "/images/matchalate.webp"
+  },
+  
+];
+
+// Sản phẩm chính cho Hero section
+export const mainFeaturedProduct = products[0];

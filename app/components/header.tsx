0f4dc4757d7d31f68db723019232c106b9809416
KhanhBaoLe <khanh.le230201@vnuk.edu.vn>
2025-05-28 10:02:09 +0700

    Add id table page & url

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -99,6 +99,9 @@ export default function Header() {
               <Link href="/menu" className="text-gray-600 hover:text-amber-800 transition-colors font-medium">
                 Menu
               </Link>
+              <Link href={`/table/1`} className="bg-orange-500 text-white px-4 py-2 rounded">
+                Table
+              </Link>
             </nav>
           </div>
 

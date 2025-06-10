4f06aa72e4ce199d24824972fdaf08e8e0cb3b5d
KhanhBaoLe <khanh.le230201@vnuk.edu.vn>
2025-06-10 11:26:49 +0700

    fix bug

--- app/types/jsx.d.ts
+++ app/types/jsx.d.ts
@@ -2,7 +2,7 @@
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      [elemName: string]: any;
+      [elemName: string]: any; // eslint-disable-line @typescript-eslint/no-explicit-any
     }
   }
 } 
